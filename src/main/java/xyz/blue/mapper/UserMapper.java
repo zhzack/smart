0d9e43979564ba269423e0fe56753a94f213f6a7
zhzack <317102060759@aiit.edu.cn>
2021-03-18 02:21:38 +0800

    2021年3月18日02点21分
    Signed-off-by: zhzack <317102060759@aiit.edu.cn>

---
+++ src/main/java/xyz/blue/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package xyz.blue.mapper;
+
+import xyz.blue.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer user_id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer user_id);
+
+    User selectByName(String user_name);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
